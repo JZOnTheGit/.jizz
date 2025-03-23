fd0b3d4c72fbd3862457429d15e53e69966679b5
J.S.T <jass150505@gmail.com>
2025-03-23 22:33:05 +0000

    first commit

---
+++ frontend/ast.ts
@@ -0,0 +1,103 @@
+import Environment from "../runtime/environment.ts";
+
+export type NodeType = 
+//expressions
+| "Program" 
+| "VarDeclaration"
+| "FunctionDeclaration"
+| "AssignmentExpr"
+| "MemberExpr"
+| "CallExpr"
+
+//statements
+| "NumericLiteral" 
+| "Identifier" 
+| "BinaryExpr" 
+| "UnaryExpr" 
+| "FunctionDeclaration"
+
+| "Property"
+| "ObjectLiteral";
+
+
+
+
+
+export interface Stmt {
+    kind: NodeType;
+}
+
+export interface Program extends Stmt {
+    kind: "Program";
+    body: Stmt[];
+}
+
+export interface VarDeclaration extends Stmt {
+    kind: "VarDeclaration";
+    constant: boolean,
+    identifier: string,
+    value?: Expr;
+}
+
+export interface FunctionDeclaration extends Stmt {
+    type: "function";
+    parameters: string[],
+    body: Stmt[];
+    name: string;
+    declarationEnv: Environment;
+}
+
+//expression
+
+export interface Expr extends Stmt {}
+
+export interface AssignmentExpr extends Expr {
+    kind: "AssignmentExpr",
+    assigne: Expr,
+    value: Expr,
+}
+
+export interface BinaryExpr extends Expr {
+    kind: "BinaryExpr";
+    left: Expr;
+    right: Expr;
+    operator: string;
+}
+
+export interface CallExpr extends Expr {
+    kind: "CallExpr";
+    caller: Expr;
+    args: Expr[];
+}
+
+export interface MemberExpr extends Expr {
+    kind: "MemberExpr";
+    object: Expr;
+    property: Expr;
+    computed: boolean;
+}
+
+export interface Identifier extends Expr {
+    kind: "Identifier";
+    symbol: string;
+}
+
+export interface NumericLiteral extends Expr {
+    kind: "NumericLiteral";
+    value: number;
+}
+
+export interface Property extends Expr {
+    kind: "Property";
+    key: string,
+    value: Expr,
+}
+
+export interface ObjectLiteral extends Expr {
+    kind: "ObjectLiteral";
+    properties: Property[]
+}
+
+
+
+
